af77d6f90c94035e23f542ae983b1fffb32a7780
Harshita <harshitak1204@gmail.com>
2026-03-06 13:40:19 +0530

    practice

---
+++ indexed_array.php
@@ -0,0 +1,5 @@
+<?php
+$a = array(10,20,'apple');
+echo " $a[0] \n" ;
+echo " $a[1] \n" ;
+echo " $a[2] \n" ;
---
+++ print_r_inIndexedArray.php
@@ -0,0 +1,6 @@
+<?php
+$a = array(10,20,13,'ball');
+print_r($a);
+
+$b = array(20,34,54,87,'apple');
+var_dump($b);
