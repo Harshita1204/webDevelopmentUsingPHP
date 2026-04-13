bf1004234de8d350d512050451d8ca6339575b5e
Harshita <harshitak1204@gmail.com>
2026-04-13 13:27:43 +0530

    php

---
+++ db1.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "practice_db";
+$conn = mysqli_connect($servername,$username,$password,$database);
+if(!$conn){
+    die("connection failed".mysqli_conneect_error());
+}
+else{
+    echo"connection successful";
+}
+?>
