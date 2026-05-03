a4016edc37a3256c9b5697054ae097477696bfa2
Harshita <harshitak1204@gmail.com>
2026-05-03 20:04:34 +0530

    notes

--- pdf.php
+++ pdf.php
@@ -16,14 +16,12 @@ $pdf->SetFillColor(255,182,193); // light pink
 
 // Header
 $pdf->SetFont('Arial','B',12);
-
 $pdf->Cell(60,10,'Name',1,0,'C',true);
 $pdf->Cell(40,10,'Age',1,0,'C',true);
 $pdf->Cell(60,10,'City',1,1,'C',true);
 
 // Data
 $pdf->SetFont('Arial','',12);
-
 $data = [
     ['Harshu', '22', 'Delhi'],
     ['Rinku', '25', 'Mumbai'],
