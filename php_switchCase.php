95295e238f8a1c1c1770ad1521841691a5d6fcc6
Harshita <harshitak1204@gmail.com>
2026-05-15 08:47:25 +0530

    php

---
+++ php_switchCase.php
@@ -0,0 +1,25 @@
+<?php 
+$day = "Monday"; 
+switch ($day) { 
+case "Monday": 
+echo "Start of the work week!"; 
+break; 
+case "Tuesday": 
+echo "Keep the momentum going!"; 
+break; 
+case "Wednesday": 
+echo "Midweek - you're halfway there!"; 
+break; 
+case "Thursday": 
+echo "Almost Friday!"; 
+break; 
+case "Friday": 
+echo "Last day of the work week!"; 
+break; 
+case "Saturday": 
+echo "Enjoy the weekend!"; 
+break; 
+case "Sunday": 
+echo "Relax and recharge!"; 
+break; 
+default: echo "Invalid day!"; } ?>
