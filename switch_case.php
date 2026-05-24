38653c2ec3bbb7332786bfa6217b9eb0632c5f65
Harshita <harshitak1204@gmail.com>
2026-05-24 23:48:48 +0530

    php

---
+++ switch_case.php
@@ -0,0 +1,19 @@
+<?php
+
+$day = 2;
+
+switch($day)
+{
+    case 1:
+        echo "Monday";
+        break;
+
+    case 2:
+        echo "Tuesday";
+        break;
+
+    default:
+        echo "Invalid";
+}
+
+?>
